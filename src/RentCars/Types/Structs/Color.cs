529c4bf34c0810a49a623527266a1f60409cff0a
ptSouthier <patrick.southier@hotmail.com>
2024-03-08 19:49:56 -0300

    feat: creating the Color struct according to Req 1 specs

--- src/RentCars/Types/Structs/Color.cs
+++ src/RentCars/Types/Structs/Color.cs
@@ -3,5 +3,12 @@ namespace RentCars.Types;
 //1 - Crie uma `Struct` para as **Cores**
 public struct Color
 {
-  
+  public string Name;
+  public string Hex;
+
+  public Color(string name, string hex)
+  {
+    Name = name;
+    Hex = hex;
+  }
 }
